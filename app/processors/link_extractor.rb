5cfe10907624f7a2b34af152002116900a8aad03
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-19 21:20:02 -0500

    Only include external links in link extraction

--- app/processors/link_extractor.rb
+++ app/processors/link_extractor.rb
@@ -1,10 +1,16 @@
+require 'uri'
+
 class LinkExtractor
 
   class << self
 
     def external(html)
       doc = Nokogiri::HTML.parse(html)
-      doc.css('a').map { |link| link['href'] }.uniq
+      links = doc.css('a').map { |link| link['href'] }.uniq
+      links.reject { |url|
+        host = URI(url).host
+        host.nil? || host == ENV['HOST']
+      }
     end
   end
 end
