ba18e911c055acfdc6135ff690eb333809c6400b
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-21 16:22:38 -0500

    Add better monitoring endpoint

--- app/controllers/monitors_controller.rb
+++ app/controllers/monitors_controller.rb
@@ -4,10 +4,27 @@ class MonitorsController < ApplicationController
     respond_to do |format|
       format.json do
         render :json => {
-          'all' => true
+          'db' => test_db
         }
       end
-      format.html { render :ok }
+      format.html do
+        test_all
+        render layout: false
+      end
     end
   end
+
+  def test_all
+    test_db
+  end
+
+  def test_db
+    Article.first
+    true
+  end
+
+  # def test_cache
+  #   raise "CacheCheckFail" unless !Rails.cache.exist?('monitors-heartbeat-foobar')
+  #   true
+  # end
 end
