c6174202cea2320c2f79e9a2d88a3ff719989184
zeke <zeke@sikelianos.com>
2012-11-21 20:05:28 -0800

    hamlbars templating, repo man for fetching github repo info, compass for css3 sass mixins.

--- Gemfile
+++ Gemfile
@@ -3,22 +3,23 @@ source 'https://rubygems.org'
 gem 'rake'
 gem 'rails'
 gem 'thin'
-
 gem 'pg'
 gem 'activerecord-postgres-hstore'
 gem 'scrolls'
 gem 'lograge'
 gem 'airbrake'
 gem 'newrelic_rpm'
-
+gem 'haml-rails'
+gem 'stamp'
 gem 'curb'
 gem 'redcarpet'
-
-gem 'haml-rails'
 gem 'jquery-rails'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails'
+  gem 'zazz'
+  gem 'handlebars_assets'
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Adj
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Recommended for https://github.com/leshill/handlebars_assets
+    config.assets.initialize_on_precompile = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
