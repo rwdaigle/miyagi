fa80d4496cde403b6372cb571023c44adf9c779b
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-22 21:20:51 -0500

    Track subscriptions

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
 
   def follow
-    Scrolls.log(measure: "adj.links.clicked", from: request.referer, to: params[:url], context: params[:context])
+    Scrolls.log(measure: "adj.link.clicks", from: request.referer, to: params[:url], context: params[:context])
     redirect_to params[:url]
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def subscribe
     current_user.subscribe!(params[:email])
+    Scrolls.log(measure: "adj.user.subscriptions", from: request.referer)
+    register_event(:subscribed) # This doesn't seem to work if subscribing from the first page view (?)
     respond_to do |format|
       format.html { redirect_to :back }
     end
