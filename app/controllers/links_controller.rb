50521b50876ce448b11635da1c52a4f1a7e7dc12
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-28 20:35:53 -0500

    Rename from adj to miyagi

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
 
   def follow
-    Scrolls.log(measure: "adj.link.clicks", from: request.referer, to: params[:url], context: params[:context])
+    Scrolls.log(measure: "miyagi.link.clicks", from: request.referer, to: params[:url], context: params[:context])
     redirect_to params[:url]
   end
 end
