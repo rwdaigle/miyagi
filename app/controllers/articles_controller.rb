004a54e57c4cb7434f6f90eecb96e0d10bec2f91
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-28 21:26:49 -0500

    Track homepage vs. article views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+  before_filter :register_content_viewed_event, :only => :show
+
   def show
     @article = Article.published.find(params[:id])
     # fresh_when(@article)
+  end
+
+  private
+
+  def register_content_viewed_event
+    register_event(:content_viewed)
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,15 @@
 class HomesController < ApplicationController
 
+  before_filter :register_homepage_viewed_event, :only => :index
+
   def index
     @articles = Article.published.recent.limit(10)
     # fresh_when(last_modified: @articles.maximum(:updated_at))
+  end
+
+  private
+
+  def register_homepage_viewed_event
+    register_event(:homepage_viewed)
   end
 end
