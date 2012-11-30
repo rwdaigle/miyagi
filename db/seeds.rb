f156db799b306012385a3767cc1f38293ea50d54
Ryan Daigle <ryan.daigle@gmail.com>
2012-11-30 17:05:40 -0500

    Add accurate content summaries

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,17 @@ authors.each do |author_params|
   puts m.errors.full_messages.join(', ') if !m.valid?
 end
 
+abd_summary = "Automated testing is a proven tool for verifying application behavior and preventing bugs and regressions. However, as a last-minute line of defense, testing can often be eschewed in favor of baking preventative behavior directly into the application design."
+ppds_summary = "Assigning a group of worker processes to operate, in parallel, against a large dataset is a very efficient approach to data processing. Process partitioning is a scalable approach for coordinating work across a pool of independent workers."
+
 articles = [
-  ["ped", "Assertions by Design", "https://raw.github.com/gist/bf2e720a2d045cd70725/assertions.md"],
-  ["ryandotsmith", "Process Partitioning in Distributed Systems", "https://raw.github.com/gist/4127005/process-partitioning.md"]
+  ["ped", "Assertions by Design", "https://raw.github.com/gist/bf2e720a2d045cd70725/assertions.md", abd_summary],
+  ["ryandotsmith", "Process Partitioning in Distributed Systems", "https://raw.github.com/gist/4127005/process-partitioning.md", ppds_summary]
 ]
 
 articles.each do |content|
   puts "Fetching #{content[1]} content..."
   req = Curl.get(content[2])
-  m = Article.create(:author => User.find_by_twitter_username(content[0]), :title => content[1], :body => req.body_str, :published_at => Time.now)
+  m = Article.create(:author => User.find_by_twitter_username(content[0]), :summary => content[3], :title => content[1], :body => req.body_str, :published_at => Time.now)
   puts m.errors.full_messages.join(', ') if !m.valid?
 end
