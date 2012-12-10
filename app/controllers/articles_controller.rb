e1f5142e449c77529262cbe6d567653326930922
Ryan Daigle <ryan.daigle@gmail.com>
2012-12-10 14:52:21 -0500

    Add article ATOM feed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,13 @@ class ArticlesController < ApplicationController
 
   before_filter :register_content_viewed_event, :only => :show
 
+  def index
+    @articles = Article.published.recent.includes(:author).limit(10)
+    respond_to do |format|
+      format.atom
+    end
+  end
+
   def show
     @article = Article.published.includes(:author).find(params[:id])
     # fresh_when(@article)
